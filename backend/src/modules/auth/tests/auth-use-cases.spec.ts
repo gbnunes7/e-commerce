d1bb2e91567c6e2e88db8c2002097ceafccc22d7
Gabriel Melo <bielrj170@gmail.com>
2025-01-30 00:21:57 -0300

    feat: create a tests to auth use cases

---
+++ backend/src/modules/auth/tests/auth-use-cases.spec.ts
@@ -0,0 +1,73 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import InMemoryAuthRepository from "../repository/in-memory-auth-repository";
+import { AuthUseCases } from "../use-cases";
+import { compare } from "bcrypt";
+import { UserAlreadyExistsError } from "../errors/user-already-exists-error";
+
+let authRepository: InMemoryAuthRepository;
+let sut: AuthUseCases;
+
+describe("Auth Use Cases", () => {
+  beforeEach(() => {
+    authRepository = new InMemoryAuthRepository();
+    sut = new AuthUseCases(authRepository);
+  });
+
+  it("should hash user password when register", async () => {
+    const user = await sut.createUser({
+      email: "any_email",
+      name: "any_name",
+      password: "any_password",
+      role: "ADMIN",
+    });
+
+    const isPasswordHashed = await compare("any_password", user.password);
+
+    expect(isPasswordHashed).toBe(true);
+  });
+
+  it("should throw error if user already exists", async () => {
+    await sut.createUser({
+      email: "any_email",
+      name: "any_name",
+      password: "any_password",
+      role: "ADMIN",
+    });
+
+    await expect(
+      sut.createUser({
+        email: "any_email",
+        name: "any_name",
+        password: "any_password",
+        role: "ADMIN",
+      })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
+  });
+
+  it("should register a user", async () => {
+    const user = await sut.createUser({
+      email: "any_email",
+      name: "any_name",
+      password: "any_password",
+      role: "ADMIN",
+    });
+
+    expect(user.email).toBe("any_email");
+  });
+
+  it("should authenticate a user", async () => {
+    const user = await sut.createUser({
+      email: "any_email",
+      name: "any_name",
+      password: "any_password",
+      role: "ADMIN",
+    });
+
+    const authenticatedUser = await sut.login({
+      email: "any_email",
+      password: "any_password",
+    });
+
+    expect(authenticatedUser.email).toBe(user.email);
+  });
+});
